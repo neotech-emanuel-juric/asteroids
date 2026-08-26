5956812d3e8a2b1563edcdb93f7185ba008ac286
emanuel.juric <emanuel.juric@neo-tech-solutions.de>
2026-08-26 17:49:03 +0200

    add delta time and circleshape class for future sprites

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    gameClock = pygame.time.Clock() # new clock object for fps
+    dt = 0.0 # Delta
 
     while True:
         log_state()
@@ -13,7 +15,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-    
+        dt = gameClock.tick(60) / 1000 # tick() method returns the amount of time that has passed since the last time it was called: the delta time
 
 
 if __name__ == "__main__":
